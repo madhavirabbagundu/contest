4b412111f07c822195c5d7dab65fe59a6d523b23
Madhavi Rabbagundu <madhavirabbagundu@gmail.com>
2023-04-07 23:58:50 +0530

    palindrome

--- project/object.js
+++ project/object.js
@@ -4,3 +4,21 @@ var obj = {
     a:30
 }
 console.log(obj)
+
+
+function palindrome(str){
+    var out = ""
+    for(var i = str.length-1; i>= 0; i--){
+        out+=str[i]
+      
+    }
+    console.log(str,out)
+    if(str === out){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+console.log(palindrome("malayalam"))
